e7437d7d5fffb49c67c0ffbdeea53d0480259b7b
Tyler Daerden <daerdentyler@gmail.com>
2023-03-27 19:13:07 +0200

    Ajoutez des fichiers projet.

---
+++ Exos28032023/Program.cs
@@ -0,0 +1,79 @@
+﻿#region Exos Boucles
+
+#region Exercice 1.1  Fibonacci OK ! 
+//Calculer les 25 premiers nombres de la suite de Fibonacci
+//int nb1 = 0;
+//int nb2 = 1;
+
+//Console.WriteLine(nb1);
+//Console.WriteLine(nb2);
+//for (int i = 0; i < 23; i++) //on s'arrête plus tôt car nous avons les 2 premières valeurs
+//{
+//    int nbfibo = (nb1 + nb2);
+//    Console.WriteLine(nbfibo);
+//    nb1 = nb2;
+//    nb2 = nbfibo;
+
+//}
+
+#endregion
+
+#region Exercice 1.2 Factoriel OK ! 
+//Calculer le factoriel d’un nombre entré au clavier.
+
+//Console.WriteLine("De quel nombre voulez vous calculer la factorielle ?  ");
+//int UserChoix = int.Parse(Console.ReadLine());
+//int factorielle = 1;
+
+//for (int i = 1 ; i <= UserChoix ; i++)
+//{
+//    factorielle*= i;
+//}
+
+//Console.WriteLine($"la Factorielle de {UserChoix} est {factorielle}");
+
+#endregion
+
+#region Exercice 1.3 Nombres Premiers  A FINIR !
+//Grâce à une boucle « for », calculez les x premiers nombre premier
+
+#endregion
+
+#region Exercice 1.4 Tables multiplication OK !
+//A l’aide de boucles « for » afficher les 5 premières tables de multiplication en allant jusque « x20 ».
+//1x1 = 1 ; 2x1 = 2; ……
+//2x1 = 2; 2x2 = 4; ……
+
+int tablede = 1;
+
+for (int i = 1; i <= 5; i++)
+{
+    for (int multiplicateur = 1; multiplicateur <= 20; multiplicateur++)
+    {
+        Console.WriteLine($"{tablede} * {multiplicateur} = " + (tablede * multiplicateur));
+
+    }
+    tablede++;
+}
+
+#endregion
+
+#region Exercice 1.5 boucle Doubles OK !
+//À l’aide d’une boucle « for » comptez de 0, à 20,0 en augmentant de 0,1, en utilisant des doubles, et afficher la valeur à chaque itération.
+//Remarquez-vous quelque chose de particulier ?
+
+//for (double i = 00; i <=20.0; i=i+0.1)
+//{
+//    Console.WriteLine(i);
+//}
+// on arrive pas à 20 et régulièrement on passe de une à 10 décimale
+
+#endregion
+
+#region Exercice 1.6 racine carrée  A FINIR
+//Bonus: Calculer la racine carré d’un nombre avec maximum 10 décimales (Math.Sqrt(x) ne peut être utilisée que pour vérifier la réponse),
+
+
+#endregion
+
+#endregion
